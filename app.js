8bd023b6096116fd12c05cb813dd3097bf21b06b
Ali Hassan Liaquat <liaqqahl@gmail.com>
2025-09-03 10:52:30 +0100

    Serve static index file from server to browser

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'html');
 app.listen(port);
 
 app.get('/', (req, res) => {
-    console.log('Request Made!');
+    res.sendFile(__dirname + '/views/index.html');
 })
