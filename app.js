7d5c5a740ce535d69ed4dd8f574ef67ecb0aaf45
Ali Hassan Liaquat <liaqqahl@gmail.com>
2025-09-05 16:36:17 +0100

    Add style.css and background image for space view

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'))
 app.listen(port);
 
 app.get('/', (req, res) => {
     res.render('index', {title: 'Home Page'});
+})
+
+app.get('/space', (req, res) => {
+    res.render('space', {title: 'Space Page'});
 })
